d63a07d7b4dca3adec97f01a88e121e403542e0f
Ramesh Ganesan <ramesharavinth@gmail.com>
2023-07-20 20:19:49 +0530

    Update read_files.sh

--- read_files.sh
+++ read_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Assuming you have cloned the repository, change to the repository's root directory
-cd /path/to/your/git/repo
+cd /
 
 # Get the folder name from the first argument passed to the script
 folder_name="$1"
